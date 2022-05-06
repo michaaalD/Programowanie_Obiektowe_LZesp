1170bab40d2ba8dd01e159a6c7047c13a7a75835
Michał Doś <93281456+michaaalD@users.noreply.github.com>
2022-05-06 21:09:34 +0200

    Add files via upload

---
+++ StatystykaTestu.cpp
@@ -0,0 +1,10 @@
+#include "StatystykaTestu.hh"
+#include <iostream>
+
+
+/*
+void Oblicz_Staty( StatystykaTestu  &StatTestu )
+{
+    StatTestu.procent_poprawnych = (StatTestu.ile_poprawnych/StatTestu.ile_pytan) * 100;
+}
+*/
